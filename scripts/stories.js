134948e860f04a2541264966da2ee271023976c3
Jacques007 <kdubbelos@gmail.com>
2025-12-23 16:06:06 +0200

    Create stories.js

---
+++ scripts/stories.js
@@ -0,0 +1,12 @@
+  fetch('Stories.txt')
+      .then(r => {
+        if (!r.ok) throw new Error('Failed to load content.txt');
+        return r.text();
+      })
+      .then(text => {
+        document.getElementById('content').textContent = text;
+      })
+      .catch(err => {
+        document.getElementById('content').textContent = 'Could not load content.';
+        console.error(err);
+      });
