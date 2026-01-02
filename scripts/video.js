4ee08c5b588c54cedc20c18779fead8b4ca2bb17
Jacques007 <kdubbelos@gmail.com>
2026-01-02 13:55:05 +0200

    Update video.js

--- scripts/video.js
+++ scripts/video.js
@@ -23,12 +23,12 @@ fetch('json/video.json')
 
     if (visibleVideos.length === 0) {
       visibleVideos = videos.filter(v => v.date < todayStr).slice(-3);
-      if (visibleVideos.length > 0) {
-        const note = document.createElement('p');
-        note.className = 'note';
-        note.innerText = 'Here are your recent messages';
-        container.appendChild(note);
-      }
+     // if (visibleVideos.length > 0) {
+     //   const note = document.createElement('p');
+     //   note.className = 'note';
+     //   note.innerText = 'Here are your recent messages';
+     //   container.appendChild(note);
+     // }
     }
 
     if (visibleVideos.length === 0) {
